3908abbcf7c9a742dd909315619c15b1fe807573
unknown <saiswaroop.bedamatta@gmail.com>
2023-05-24 20:21:32 +0530

    Minor update in about - 2023 - v1.0.0.2

--- src/Home/About.js
+++ src/Home/About.js
@@ -11,25 +11,25 @@ const SkillSet = () => {
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/html-5--v1.png"
-                alt=""
+                alt="HTML"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/css3.png"
-                alt=""
+                alt="CSS"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/bootstrap.png"
-                alt=""
+                alt="BOOTSTRAP"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/javascript--v1.png"
-                alt=""
+                alt="JAVASCRIPT"
               />
             </div>
           </div>
@@ -37,25 +37,25 @@ const SkillSet = () => {
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/react-native.png"
-                alt=""
+                alt="REACT JS"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/angularjs.png"
-                alt=""
+                alt="ANGULAR"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/nodejs.png"
-                alt=""
+                alt="NODE"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/java-coffee-cup-logo--v1.png"
-                alt=""
+                alt="CORE JAVA"
               />
             </div>
           </div>
@@ -63,19 +63,19 @@ const SkillSet = () => {
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/amazon-web-services.png"
-                alt=""
+                alt="AWS"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/api-settings.png"
-                alt=""
+                alt="REST APIS"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/github--v1.png"
-                alt=""
+                alt="GIT"
               />
             </div>
           </div>
@@ -83,19 +83,19 @@ const SkillSet = () => {
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/visual-studio-code-2019.png"
-                alt=""
+                alt="VSCODE"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/android-studio--v2.png"
-                alt=""
+                alt="ANDROID STUDIO"
               />
             </div>
             <div className="card">
               <img
                 src="https://img.icons8.com/color/30/000000/adobe-illustrator--v1.png"
-                alt=""
+                alt="ILLUSTRATOR"
               />
             </div>
           </div>
@@ -114,12 +114,11 @@ const About = () => {
         <div className="description">
           I'm an illustrator, designer, and aspiring full stack developer. In
           2021, I completed by gradutation in Computer Science and Engineering
-          with 8.75 cgpa. And currently working as a Full Stack developer in
-          Cognizant. When I'm not drawing, creating web-pages, and doing all my
-          design-related interests, you'll find me outside with my friends in a
-          calm place, reading science fiction/non-fiction, making memes,
-          managing social-media pages and drinking as much tea as physically
-          possible.
+          with 8.75 cgpa. And currently working as a Full Stack developer. When
+          I'm not drawing, creating web-pages, and doing all my design-related
+          interests, you'll find me outside with my friends in a calm place,
+          reading science fiction/non-fiction, making memes, managing
+          social-media pages and drinking as much tea as physically possible.
         </div>
         <SkillSet />
       </div>
